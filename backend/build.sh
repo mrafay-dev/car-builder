86558d7987c786d74da7b59f2315819154a3f71c
mrafay-dev <mrafay333@gmail.com>
2025-09-09 14:58:48 +0100

    fixing yarn error

--- backend/build.sh
+++ backend/build.sh
@@ -4,12 +4,6 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Move to the frontend folder one level up
-cd ../frontend
-npm install
-npm run build
-cd ../backend
-
 # Collect static files
 python manage.py collectstatic --noinput
 
