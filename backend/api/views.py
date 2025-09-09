ade703cbc6187042843e2afceacc7e091d76b5f2
mrafay-dev <mrafay333@gmail.com>
2025-09-09 14:23:06 +0100

    dist folder

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Car
 from .serializers import CarSerializer
+# backend/views.py
+from django.views.generic import TemplateView
+
+class FrontendAppView(TemplateView):
+    template_name = "index.html"
+
 
 class CarListCreate(generics.ListCreateAPIView):
     queryset = Car.objects.all()
